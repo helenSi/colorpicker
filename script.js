505f4a3824ba204f58b23b4bf1c08e73de8f5cde
Jongjai Lochman <jongfreedom@gmail.com>
2015-03-29 16:30:26 +0100

    function click preview item

---
+++ script.js
@@ -0,0 +1,67 @@
+/* Exercise 2: Color picker */
+var colors = ["brown" ,"purple", "orange", "#091287", "green", "red" ,"blue", "yellow"];
+	random_position = Math.floor(Math.random() * colors.length);
+	
+
+// functions' name
+function setPreviewColor(color){
+	$(".preview").css("background", color);
+};
+
+function addBox(color){
+
+	$("#colors").prepend("<div class ='item' style='background:" +color +"'></div>" );
+	$("#color").val("");
+};
+
+function removeBox(color){
+	$(".item:last-child").remove();
+	colors.shift(colors[0])
+	colors.push(color);
+};
+// preset colors
+
+$(document).ready(function(){
+
+	//Random set color on the LEFT when the page is load in the first time
+	$(".preview").ready(function(){
+		$(".preview").css("background-color",colors[random_position]);
+		$(".color-code").text($(".preview").css("background-color"));
+	});
+
+	//set colors on the RIGHT when the page is load on the first time
+	$.each(colors, function(index,color){
+		addBox(color);
+	});
+});
+
+// change color(left)
+$(document).on('keyup', '#color', function(){
+	var color = $("input#color").val();
+
+	setPreviewColor(color);
+	$(".color-code").text($(".preview").css("background-color"));
+});
+
+
+// add colors favourit(right)
+$(document).on('click', '#add-to-favorite', function(){
+	var color = $("input#color").val();
+
+	if(colors.length < 16){
+		addBox(color);
+		colors.push(color);
+	}else{
+		removeBox(color);
+		addBox(color);
+	}
+	$("#color").focus();
+
+});
+
+// Mouse over view the color
+$(document).on('click', '.item', function(){
+	$(".preview").css("background-color", $(this).css("background-color"));
+	$(".color-code").text($(this).css("background-color"));
+});
+
